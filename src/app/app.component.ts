9dc3514f0fb8ec336bbae085c8121303d2f83d73
Oldschool69 <flavio.marcondes@gmail.com>
2025-05-03 10:19:34 -0300

    finalizando o modulo de forms utilizando template e reactive forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component, OnInit } from '@angular/core';
 
 import { LoginComponent } from './auth/login/login.component';
 import { LoginReactiveComponent } from './auth/login-reactive/login-reactive.component';
+import { SignupComponent } from "./auth/signup/signup.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [LoginComponent, LoginReactiveComponent],
+  imports: [LoginComponent, LoginReactiveComponent, SignupComponent],
 })
 export class AppComponent {
 
